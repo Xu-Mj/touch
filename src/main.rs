28b7ccf615115133eaac2b5cfec15ee78fa24f89
xu_mj <331538695@qq.com>
2024-02-22 21:12:55 +0800

    feat: add installation command -i/--install

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::{self, Write}, path::Path};
+use std::{
+    fs::File,
+    io::{self, Write},
+    path::Path,
+};
 
 use clap::{arg, command, Arg, ArgAction};
 
@@ -6,9 +10,10 @@ fn main() {
     let matches = command!()
         .version("0.1.0")
         .author("Xu-Mj")
-        .about("`touch` command on windows") 
+        .about("`touch` command on windows")
         .arg(Arg::new("filename").action(ArgAction::Append))
         .arg(arg!(-f --force "force overwrite").action(ArgAction::SetTrue))
+        .arg(arg!(-i --install "installation").action(ArgAction::SetTrue))
         .get_matches();
 
     let args = matches
@@ -16,6 +21,11 @@ fn main() {
         .unwrap_or_default()
         .map(|v| v.as_str())
         .collect::<Vec<_>>();
+
+    if matches.get_flag("install") {
+        // install to PATH
+        return install();
+    }
 
     let mut force = false;
     if matches.get_flag("force") {
@@ -45,4 +55,52 @@ fn main() {
             }
         }
     }
+}
+
+fn install() {
+    println!("[INFO]: `touch` is installing...");
+    // get current dir
+    match std::env::current_dir() {
+        Ok(path) => {
+            let path = path.display();
+            println!("[INFO]: install path: {}", path);
+            let command = format!(
+                r#"
+                $addPath='{path}';
+                $target='Machine'; 
+                $path = [Environment]::GetEnvironmentVariable('Path', $target); 
+                if($path -match ";$")
+                    {{ $newPath = $path + $addPath; }} 
+                else 
+                    {{ $newPath = $path + ';' + $addPath; }} 
+                [Environment]::SetEnvironmentVariable('Path', $newPath, $target)"#
+            );
+            // add path to evironment variable
+            println!("[INFO]: add current dir to environment variable ...");
+            match std::process::Command::new("powershell.exe")
+                .arg(command)
+                .output()
+            {
+                Ok(res) => {
+                    // run command success
+                    if res.status.success() {
+                        println!("[INFO]: installation success!");
+                        println!("[INFO]: congratulations!");
+                        println!("[INFO]: please restart your terminal");
+                    } else {
+                        // run command failed
+                        println!("[ERROR]: execute installation command failed!");
+                        println!("[INFO]: maybe you should run `touch -i/--install` with administrator privilege");
+                    }
+                }
+                Err(_) => {
+                    println!("[ERROR]: execute installation command failed!");
+                    println!("[INFO]: maybe you should run `touch -i/--install` with administrator privilege");
+                }
+            }
+        }
+        Err(err) => {
+            println!("[ERROR]: get current dir failed: {err}");
+        }
+    };
 }
